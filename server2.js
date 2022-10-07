227fc47f9dbea547737e6cbbdc563b740eebeece
Michael Wood <michaelwood33311@icloud.com>
2022-10-07 09:16:13 -0400

    added uploading contents to s3 bucket and erasing all bucket contents

--- server2.js
+++ server2.js
@@ -12,8 +12,6 @@ const client = new S3({
     }
 });
 
-console.log(process.env.SPACES_KEY);
-console.log(CreateBucketCommand);
 
 // Specifies the new Space's name.
 
@@ -29,6 +27,69 @@ const createNewBucket = async (bucket, s3Client) => {
     console.log("Error", err);
   }
 };
+
+// upload local file to S3
+
+const upload = async (fileName, documentId, extension) => {
+  const data = await fsp.readFile(fileName);
+  let contentType = '';
+
+  switch (extension.toLowerCase()) {
+    case '.docx':
+        contentType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+        break;
+    case '.pdf':
+        contentType = 'application/pdf'
+        break;
+
+  }
+
+  const bucketParams = {
+      Bucket: process.env.S3_BUCKET,
+      Key: `${documentId}/${documentId}${extension}`,
+      Body: data,
+      ACL: 'public-read',
+      'Content-Type': contentType
+    };
+  
+    try {
+      const data = await s3Client.send(new PutObjectCommand(bucketParams));
+      const link = `https://${process.env.S3_BUCKET}.${process.env.S3_ENDPOINT_DOMAIN}/${bucketParams.Key}`;
+      return link;
+    } catch (err) {
+      console.log("Error", err);
+      return '';
+    }
+      
+};
+
+// erase all S3 files in directory
+
+const eraseS3Contents = async (folder) => {
+  const listParams = {
+      Bucket: process.env.S3_BUCKET,
+      Prefix: `${folder}/`
+  };
+
+  const listedObjects = await s3Client.send(new ListObjectsV2Command(listParams));
+
+  if (!listedObjects.Contents) return;
+  if (listedObjects.Contents.length === 0) return;
+
+  const deleteParams = {
+      Bucket: process.env.S3_BUCKET,
+      Delete: { Objects: [] }
+  };
+
+  listedObjects.Contents.forEach(({ Key }) => {
+      deleteParams.Delete.Objects.push({ Key });
+  });
+
+  await s3Client.send(new DeleteObjectsCommand(deleteParams));
+
+  if (listedObjects.IsTruncated) await eraseS3Contents(folder);
+}
+
 
 const emptyS3Directory = async (bucket, dir, s3Client) => {
   const listParams = {
